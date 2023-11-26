283a2209587e4bc09e0489979fcb0fc907172829
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-11-26 18:54:31 +0300

    delete user and update the profile by the owner middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -27,3 +27,23 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
       return res.sendStatus(500);
     }
   }
+
+  export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+      const { id } = req.params;
+      const currentUserId = get(req, 'identity._id') as string;
+  
+      if (!currentUserId) {
+        return res.sendStatus(400);
+      }
+  
+      if (currentUserId.toString() !== id) {
+        return res.sendStatus(403);
+      }
+  
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+  }
