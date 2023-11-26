2855a9cfac9b5232ea01a670ba330b13133aa88a
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-11-26 18:23:59 +0300

    create login controller and handle login route

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,7 +1,8 @@
-import { register } from '../controllers/auth';
+import { register, login } from '../controllers/auth';
 import express from 'express';
 
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
+    router.post('/auth/login', login);
   };
